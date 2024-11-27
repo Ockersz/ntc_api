2df6a911274aa0a9530c56949b55ae6a58bb8d6f
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-11-27 19:36:52 +0530

    refactor(auth): remove unused entity and DTO files; update app module for global config

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config accessible globally
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
