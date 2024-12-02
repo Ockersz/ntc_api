c70ac98c3d0a342ae6b2a2f98e22d50fb20e6349
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-02 19:40:18 +0530

    feat(auth): integrate TypeORM for MySQL support; add global validation pipe and remove DynamoDB module

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Makes the config accessible globally
+      isGlobal: true, // Makes the .env variables accessible across the app
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'ntc',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     AuthModule,
   ],
