ca9d61c3f219e38a4c705a86ea8f87a1996c5eec
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-02 19:40:49 +0530

    feat(auth): update User entity to include first name, last name, and status fields; change userId to auto-generated number

--- auth-service/src/auth/entities/user.entity.ts
+++ auth-service/src/auth/entities/user.entity.ts
@@ -1,7 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
 export class User {
-  userId: string; // Unique identifier (UUID or similar)
-  username: string; // Username of the user
-  email: string; // User's email address
-  passwordHash: string; // Hashed password
-  createdAt: string; // Timestamp of account creation
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column({ length: 50 })
+  firstName: string;
+
+  @Column({ length: 50 })
+  lastName: string;
+
+  @Column({ unique: true, length: 100 })
+  email: string;
+
+  @Column({ unique: true, length: 50 })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'tinyint', default: 1 })
+  status: number; // Active (1) or Inactive (0)
 }
