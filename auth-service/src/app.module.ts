f7e70f968d5b61c45594895bd82857b2224002ad
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-03 23:28:57 +0530

    feat(auth): add throttling functionality with configuration for short, medium, and long limits

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/entities/user.entity';
@@ -9,6 +10,23 @@ import { User } from './auth/entities/user.entity';
     ConfigModule.forRoot({
       isGlobal: true, // Makes the .env variables accessible across the app
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
       host: process.env.DB_HOST,
