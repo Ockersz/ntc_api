95f6e9f28130a0357c1f1c400b7eac37e5edd440
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-08 18:20:20 +0530

    feat(auth): add CreateUserNtcDto for user registration with validation

---
+++ auth-service/src/auth/dto/create-ntc-user.dto.ts
@@ -0,0 +1,31 @@
+import { IsArray, IsEmail, IsString } from 'class-validator';
+import { IsPassword, IsUsername } from 'src/common/dataValidator';
+
+export class CreateUserNtcDto {
+  @IsString({ message: 'First name must be a string' })
+  firstName: string;
+
+  @IsString({ message: 'Last name must be a string' })
+  lastName: string;
+
+  @IsEmail(
+    {
+      allow_ip_domain: false,
+      allow_utf8_local_part: true,
+      require_tld: true,
+    },
+    { message: 'Email must be a valid email address' },
+  )
+  email: string;
+
+  @IsUsername({
+    message: 'Username must be all lowercase and at least 7 characters long',
+  })
+  username: string;
+
+  @IsPassword()
+  password: string;
+
+  @IsArray()
+  roles: number[];
+}
