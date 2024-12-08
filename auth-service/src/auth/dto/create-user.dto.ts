8a1ac0a69e6db034ab1911970481b9fa1108c6d3
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-08 18:20:33 +0530

    feat(auth): enhance DTOs with Swagger API documentation for user registration and login

--- auth-service/src/auth/dto/create-user.dto.ts
+++ auth-service/src/auth/dto/create-user.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 import { IsPassword, IsUsername } from 'src/common/dataValidator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'John',
+    description: 'The first name of the user',
+  })
   @IsString({ message: 'First name must be a string' })
   firstName: string;
 
+  @ApiProperty({
+    example: 'Doe',
+    description: 'The last name of the user',
+  })
   @IsString({ message: 'Last name must be a string' })
   lastName: string;
 
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The email address of the user',
+  })
   @IsEmail(
     {
       allow_ip_domain: false,
@@ -18,11 +31,19 @@ export class CreateUserDto {
   )
   email: string;
 
+  @ApiProperty({
+    example: 'johndoe',
+    description: 'The username of the user',
+  })
   @IsUsername({
     message: 'Username must be all lowercase and at least 7 characters long',
   })
   username: string;
 
+  @ApiProperty({
+    example: 'password',
+    description: 'The password of the user',
+  })
   @IsPassword()
   password: string;
 }
