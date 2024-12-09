b44194876b47dc663ed7f436fdd99c6c658d5b37
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-09 22:16:14 +0530

    feat(auth): integrate RoleAccess entity and enhance permissions handling in AuthService

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleAccess } from 'src/role-access/entities/role-access.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { UserRole } from 'src/roles/entities/user-role.entity';
 import { AuthController } from './auth.controller';
@@ -18,7 +19,7 @@ import { User } from './entities/user.entity';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1hr' },
     }),
-    TypeOrmModule.forFeature([User, UserRole, Role]),
+    TypeOrmModule.forFeature([User, UserRole, Role, RoleAccess]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- auth-service/src/auth/auth.service.ts
+++ auth-service/src/auth/auth.service.ts
@@ -8,9 +8,10 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Common } from 'src/common/common';
+import { RoleAccess } from 'src/role-access/entities/role-access.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { UserRole } from 'src/roles/entities/user-role.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
@@ -24,7 +25,8 @@ export class AuthService {
     private roleRepository: Repository<Role>,
     @InjectRepository(UserRole)
     private userRoleRepository: Repository<UserRole>,
-
+    @InjectRepository(RoleAccess)
+    private roleAccessRepository: Repository<RoleAccess>,
     private jwtService: JwtService,
   ) {}
 
@@ -94,14 +96,52 @@ export class AuthService {
       relations: ['role'],
     });
 
-    console.log(roleAccess);
+    const accessIds = roleAccess.reduce((acc, curr) => {
+      return [...acc, ...curr.role.access];
+    }, []);
+
+    const uniqueAccessIds = [...new Set(accessIds)];
+
+    const roleAccessess = await this.roleAccessRepository.find({
+      where: { accessId: In(uniqueAccessIds) },
+    });
+
+    const permissions = roleAccessess.map((roleAccess) => {
+      return {
+        resource: roleAccess.resource,
+        accessType: roleAccess.accessType,
+      };
+    });
+
+    //get every unique resource and set the accessTypes to one resource
+    const resourcePermissions = permissions.reduce((acc, curr) => {
+      if (!acc[curr.resource]) {
+        acc[curr.resource] = curr.accessType;
+      } else {
+        //if accessType is already set for a resource then skip
+        const accessTypes = acc[curr.resource];
+        const newAccessTypes = curr.accessType.filter((accessType) => {
+          return !accessTypes.includes(accessType);
+        });
+
+        acc[curr.resource] = [...accessTypes, ...newAccessTypes];
+      }
+      return acc;
+    }, {});
 
     const refreshToken = await this.jwtService.signAsync(
-      { sub: user.userId, username: user.username },
+      {
+        sub: user.userId,
+        username: user.username,
+      },
       { expiresIn: '7d', secret: process.env.JWT_REFRESH_SECRET },
     );
 
-    const payload = { sub: user.userId, username: user.username };
+    const payload = {
+      sub: user.userId,
+      username: user.username,
+      permissions: resourcePermissions,
+    };
     const accessToken = await this.jwtService.signAsync(payload, {
       expiresIn: '15m',
       secret: process.env.JWT_SECRET,
