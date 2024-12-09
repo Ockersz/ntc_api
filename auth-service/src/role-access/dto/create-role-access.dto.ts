7323630cbdb80123d5e3830000a38e0f699eb252
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-09 22:18:27 +0530

    feat(role-access): enhance DTOs with Swagger API properties for role access creation and update

--- auth-service/src/role-access/dto/create-role-access.dto.ts
+++ auth-service/src/role-access/dto/create-role-access.dto.ts
@@ -1,9 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsString } from 'class-validator';
 
 export class CreateRoleAccessDto {
+  @ApiProperty({
+    example: 'Admin',
+    description: 'Name of the role',
+    required: true,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: 'users',
+    description: 'Resource to access',
+    required: true,
+  })
   @IsString()
   resource: string;
 
+  @ApiProperty({
+    example: ['read', 'write'],
+    description: 'Access type',
+    required: true,
+  })
   @IsEnum(['read', 'write', 'delete', 'update'], { each: true })
   accessType: string[]; // Use accessTypes to match the payload
 }
--- auth-service/src/role-access/dto/update-role-access.dto.ts
+++ auth-service/src/role-access/dto/update-role-access.dto.ts
@@ -1,11 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsEnum, IsString } from 'class-validator';
 import { CreateRoleAccessDto } from './create-role-access.dto';
 
 export class UpdateRoleAccessDto extends PartialType(CreateRoleAccessDto) {
+  @ApiProperty({
+    example: 'UserAllAccess',
+    description: 'Name of the role',
+    required: true,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: 'users',
+    description: 'Resource to access',
+    required: true,
+  })
   @IsString()
   resource: string;
 
+  @ApiProperty({
+    example: ['read', 'write'],
+    description: 'Access type',
+    required: true,
+  })
   @IsEnum(['read', 'write', 'delete', 'update'], { each: true })
   accessType: string[]; // Use accessTypes to match the payload
 }
