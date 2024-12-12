8d4b8cc89ec1b8f181d35fa6d0f5594f229674d0
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-12 21:39:34 +0530

    feat(schedule-management-service): add authentication middleware and schedule template routes

--- schedule-management-service/index.js
+++ schedule-management-service/index.js
@@ -3,15 +3,18 @@ const busRoutes = require("./src/bus/bus.routes");
 const sequelize = require("./src/config/database");
 const cityRoutes = require("./src/city/city.routes");
 const routeRoutes = require("./src/routes/routes.routes");
+const scheduleTemplateRoutes = require("./src/schedule-template/schedule-template.routes");
+const authMiddleware = require("./middleware/auth");
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-
+app.use(authMiddleware);
 app.use("/buses", busRoutes);
 app.use("/cities", cityRoutes);
 app.use("/routes", routeRoutes);
+app.use("/schedule-template", scheduleTemplateRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
