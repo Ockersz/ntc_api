ca4c757866f55058b531a217f04f135416a6858d
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-12 21:42:57 +0530

    feat(schedule-management-service): implement CRUD operations for schedule templates and define model relationships

---
+++ schedule-management-service/src/schedule-template/models/relations.js
@@ -0,0 +1,23 @@
+const ScheduleTemplate = require("./schedule-template.model");
+const ScheduleTemplateDetail = require("./schedule-template-detail.model");
+const Route = require("../../routes/models/routes.model");
+const Bus = require("../../bus/models/bus.model");
+
+ScheduleTemplate.hasMany(ScheduleTemplateDetail, {
+  foreignKey: "templateId",
+  onDelete: "CASCADE",
+});
+
+ScheduleTemplateDetail.belongsTo(ScheduleTemplate, {
+  foreignKey: "templateId",
+});
+
+ScheduleTemplate.belongsTo(Route, {
+  foreignKey: "routeId",
+});
+
+ScheduleTemplateDetail.belongsTo(Bus, {
+  foreignKey: "busId",
+});
+
+module.exports = { ScheduleTemplate, ScheduleTemplateDetail };
