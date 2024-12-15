713806f9e2b5b913eeb5e02d0fb1edf0cafb7046
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-16 02:21:01 +0530

    feat(notification-service): implement SQS polling for email processing and enhance email utility functions

---
+++ notification-service/emailQueue.js
@@ -0,0 +1,73 @@
+const AWS = require("aws-sdk");
+const { sendEmail } = require("./utils");
+require("dotenv").config();
+// Configure the AWS SDK
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Or your hardcoded key
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Or your hardcoded secret
+  region: process.env.AWS_REGION,
+});
+
+const sqs = new AWS.SQS();
+
+async function pollMessagesFromSQS() {
+  const params = {
+    QueueUrl: process.env.QUEUE_URL,
+    WaitTimeSeconds: 10,
+  };
+  try {
+    const data = await sqs.receiveMessage(params).promise();
+    if (data.Messages && data.Messages.length > 0) {
+      for (const message of data.Messages) {
+        // Process the message
+
+        const email = JSON.parse(message.Body);
+        const results = await sendEmail(
+          email.recipient,
+          email.subject,
+          email.body
+        );
+        // Delete the message after processing
+
+        if (results && results.status === "success") {
+          await sqs
+            .deleteMessage({
+              QueueUrl: process.env.QUEUE_URL,
+              ReceiptHandle: message.ReceiptHandle,
+            })
+            .promise();
+        } else {
+          //send an email to shaheinockersz1234@gmail.com saying that the email was not sent
+          console.log("Email not sent");
+          await sendEmail(
+            "shaheinockersz1234@gmail.com",
+            "Email not sent to " + email.recipient,
+            "The email was not sent to " +
+              email.recipient +
+              " because of the following error: " +
+              results.error
+          );
+
+          // Delete the message after processing
+          await sqs
+            .deleteMessage({
+              QueueUrl: process.env.QUEUE_URL,
+              ReceiptHandle: message.ReceiptHandle,
+            })
+            .promise();
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error receiving messages from SQS:", error.message);
+  }
+
+  // Continue polling
+  pollMessagesFromSQS();
+}
+
+// Start polling
+pollMessagesFromSQS();
+
+module.exports = { pollMessagesFromSQS };
