d73eac736f9622222dcda237fb7d6fd48809e87c
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-12-22 13:34:04 +0530

    feat(database): configure database connection using environment variables

--- booking-service/src/config/database.js
+++ booking-service/src/config/database.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 // Create a new instance of Sequelize
-const sequelize = new Sequelize("ntc", "root", "root", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_UNAME,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_TYPE,
+  }
+);
 
 module.exports = sequelize;
